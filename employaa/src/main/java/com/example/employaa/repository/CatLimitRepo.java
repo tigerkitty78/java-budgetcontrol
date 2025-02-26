5218548a948d2f3408a0290567cabc92e3e89adb
tigerkitty78 <mudaligenavithma3@gmail.com>
2025-02-26 15:35:29 +0530

    Remove deleted files from the repository

--- employaa/src/main/java/com/example/employaa/repository/CatLimitRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.employaa.repository;
-
-import com.example.employaa.entity.CategoryLimit;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatLimitRepo {
-}
